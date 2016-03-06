cbd12f7ae082a928aee0396898d0d0cbc87a21c2
Daniel Gerlach <wrack1981@gmx.de>
2016-03-06 17:32:57 +0100

    query quadtree works now

--- src/quadtree/core.clj
+++ src/quadtree/core.clj
@@ -21,6 +21,12 @@
             (< (-> box1 :nw :y) (-> box2 :se :y))
             (> (-> box1 :se :y) (-> box2 :nw :y)))))
 
+(defn leaf? [node]
+  "Returns true if given node is a leaf node."
+  (if (-> node :northWest)
+    false
+    true))
+
 (defrecord QuadTreeNode
     [boundary points
      northWest northEast
@@ -106,7 +112,21 @@
                      (insert (-> child-nodes :southEast) point)))))
 
 (defn insert-points [tree points]
+  "Convinient functions in order to insert multiple points at once."
   (reduce insert tree points))
+
+(defn query [node bounding-box]
+  "Returns all values which are contained in the given bounding box."
+  (cond (not (q-intersects? (-> node :boundary) bounding-box))
+        []
+        (not (leaf? node))
+        (concat (query (-> node :northWest) bounding-box)
+                (query (-> node :northEast) bounding-box)
+                (query (-> node :southWest) bounding-box)
+                (query (-> node :southEast) bounding-box))
+        :else
+        (filter (fn [point]
+                  (q-contains? bounding-box point)) (-> node :points))))
 
 ;; (defn -main
 ;;   "I don't do a whole lot ... yet."
